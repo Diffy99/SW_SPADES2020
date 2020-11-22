a861737b3f100784eacfd631316d3f9d5829d845
andresfsuazo <andresfsuazo@gmail.com>
2020-11-21 21:01:57 -0600

    UserData and PostGame Merge

--- Team Project/server/serverdata/UserData.java
+++ Team Project/server/serverdata/UserData.java
@@ -2,27 +2,65 @@ package server.serverdata;
 
 import ocsf.server.ConnectionToClient;
 
-public class UserData {
-
-	private String Username;
+public class UserData 
+{
+	private String username;
 	private ConnectionToClient conn;
+	private int wins;
+	private int totalGames;
 	
-	public void newUser(String Username,ConnectionToClient conn) {
-		this.Username = Username;
+	public void newUser(String Username, ConnectionToClient conn) 
+	{
+		this.username = Username;
 		this.conn = conn;
 	}
 	
-	public ConnectionToClient getConn() {
+	public void setUsername(String username) 
+	{
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setPlayAgain(String username) 
+	{
+		this.username = username;
+	}
+	
+	public String getPlayAgain() 
+	{
+		return this.username;
+	}
+	
+	public ConnectionToClient getConn() 
+	{
 		return conn;
 	}
-	public void setConn(ConnectionToClient conn) {
+	
+	public void setConn(ConnectionToClient conn) 
+	{
 		this.conn = conn;
 	}
-	public String getUsername() {
-		return Username;
-	}
-	public void setUsername(String username) {
-		Username = username;
+	
+	public void setWins(int wins) 
+	{
+		this.wins = wins;
 	}
 	
+	public int getWins() 
+	{
+		return this.wins;
+	}
+	
+	public void setTotalGames(int totalGames) 
+	{
+		this.totalGames = totalGames;
+	}
+	
+	public int getTotalGames() 
+	{
+		return this.totalGames;
+	}
 }
